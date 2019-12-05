9b8652af338c07ac335f12860876ff8f696b966f
mtrussart <trussart.m@wehi.edu.au>
2019-12-05 16:06:24 +1100

    new function praiseme

--- R/hello.R
+++ R/hello.R
@@ -13,6 +13,6 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+praiseme <- function(name) {
+  print(paste("You rock",name,sep=""))
 }
